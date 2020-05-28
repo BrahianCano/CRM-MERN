45ea5fc6fd3328ce4c84f98b8ffbd7713ad56805
Yeisson Osorno <yeisson403@gmail.com>
2020-05-28 12:40:54 -0500

    Creando Variables de entorno

--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
 'use strict';
 const Mongoose = require('mongoose');
+require('dotenv').config({ path: './variables.env' });
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const connectionString = 'mongodb://localhost:27017/hyperiot_db';
+const connectionString = process.env.CONNECTION_STRING;
 
 Mongoose.Promise = global.Promise;
 
 Mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
     
-        console.log(`Welcome , The connection with database was established in ${connectionString}`);
+        console.log(`Welcome , The connection with database was established correctly`);
         app.listen(PORT, () => console.log(`Server in NodeJS runnig and listening in port: ${PORT}`));
     }).catch(err => console.log(err));
