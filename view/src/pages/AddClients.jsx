b5cab4f6ad25a417a2e673e9f9e2c1435c7b6212
BrahianCano <brahianestiven08@gmail.com>
2020-06-14 12:17:24 -0500

    Cambio de H1 en agregar clientes

--- view/src/pages/AddClients.jsx
+++ view/src/pages/AddClients.jsx
@@ -8,7 +8,7 @@ const AddClients = () => {
 
     return (
         <main className="container mt-5">
-            <h1>Agregar un cliente</h1>
+            <h1>Agregar un cliente nuevo</h1>
             <FormClient method='POST' uri='http://localhost:4000/clientes' />
         </main>
     );
