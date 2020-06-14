797d67ce76a49f2aace7b0b2fa97fe1bc54e72d9
BrahianCano <brahianestiven08@gmail.com>
2020-06-14 12:22:41 -0500

    Cambio en el titulo H1 de la pagina de agregar usuarios

--- view/src/pages/AddClients.jsx
+++ view/src/pages/AddClients.jsx
@@ -8,7 +8,7 @@ const AddClients = () => {
 
     return (
         <main className="container mt-5">
-            <h1>Agregar un cliente nuevo a base de datos</h1>
+            <h1>Agregar un cliente nuevo a base de datos de mongo</h1>
             <FormClient method='POST' uri='http://localhost:4000/clientes' />
         </main>
     );
